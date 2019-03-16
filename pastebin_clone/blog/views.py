e2997085416d86970bfc9b8a6722fe70e8954b0c
paszkom <48290219+paszkom@users.noreply.github.com>
2019-03-15 23:17:57 -0400

    views.py
    added UserPostListView
    added import get_object_or_404
    added import User

--- pastebin_clone/blog/views.py
+++ pastebin_clone/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import  LoginRequiredMixin, UserPassesTestMixin
 
 # home url content
@@ -20,6 +21,20 @@ class PostListView(ListView):
     context_object_name = 'posts'  # define object to post for list
     ordering = ['-date_posted']  # posts will be newest top ranging to oldest bottom
     paginate_by = 10 # display the 10 most recent posts
+
+# view user posts
+class UserPostListView(ListView):
+
+    model = Post
+    template_name = 'blog/user_posts.html'  
+    context_object_name = 'posts'  # define object to post for list
+    paginate_by = 10 # display the 10 most recent posts
+
+    def get_queryset(self):
+        # if user does not exists return a 404
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 # chosen posts in detail
 class PostDetailView(DetailView):
