efcaade978d1d1a6024b7bbc57bd4f183b252539
paszkom <48290219+paszkom@users.noreply.github.com>
2019-03-15 23:19:38 -0400

    Add files via upload
    added user-posts to list of urlpatterns

--- pastebin_clone/blog/urls.py
+++ pastebin_clone/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 # maps to urls of project
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),  # path to homepage
+    path('user/<str:username>/', UserPostListView.as_view(), name = 'user-posts'),  # path to user-posts
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),  # path to unique post based on pk int
     path('post/new/', PostCreateView.as_view(), name = 'post-create'),  # path to post creation
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),  # path to post update based on pk int
